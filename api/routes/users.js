956788346b8aa48ed4720a61b6365b2c917cd6fe
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-02-26 16:33:48 +0530

    user del auth add

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import userController from "../controllers/users.js";
+import checkAuth from "../middleware/check-auth.js";
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ router.post("/signup", userController.user_signup);
 
 router.post("/login", userController.user_login);
 
-router.delete("/:userId", userController.user_delete);
+router.delete("/:userId", checkAuth, userController.user_delete);
 
 export default router;
